81a124765566d470695f822337ad8d01c4fd5511
Shubham Gupta <AVI2147520@maricopa.edu>
2018-01-05 00:21:22 +0530

    updated.

--- bot/bot.py
+++ bot/bot.py
@@ -2,7 +2,7 @@ from bittrex.bittrex import Bittrex, API_V2_0, API_V1_1
 
 
 API_KEY = 'API KEY'
-API_SECRET = 'API SECRET'
+API_SECRET = 'SECRET KEY'
 PERCENT_BUY = 0.5
 
 bittrex_obj_api1 = Bittrex(API_KEY, API_SECRET, api_version=API_V1_1)
@@ -23,11 +23,11 @@ if btc_bal['result']['Balance'] > 0:
         print('Finally, its right time to purchase as per your set value')
         invest_btc = PERCENT_BUY * btc_bal['result']['Balance']
         print(set_coin_call, invest_btc, coin_ticker_last_val)
-        #print('Investing your 50% i.e %s of btc in %s' % (str(btc_bal['result']['Balance']), set_coin_call))
-        #trade_buy_obj = bittrex_obj_api2.trade_buy(set_coin_call, order_type='LIMIT', quantity=invest_btc, rate=coin_ticker_last_val, time_in_effect='GOOD_TIL_CANCELLED')
+        trade_buy_obj = bittrex_obj_api2.trade_buy(set_coin_call, order_type='LIMIT', quantity=invest_btc, rate=coin_ticker_last_val, time_in_effect='GOOD_TIL_CANCELLED')
+        print(trade_buy_obj)
         open_order_obj = bittrex_obj_api2.get_open_orders(set_coin_call)
         print(open_order_obj)
-        if(len(open_order_obj['result']) > 0):
+        if len(open_order_obj['result']) > 0:
             open_order_uuid = open_order_obj['result'][0]['OrderUuid']
             cancel_order = bittrex_obj_api2.cancel(open_order_uuid)
             print(cancel_order)
